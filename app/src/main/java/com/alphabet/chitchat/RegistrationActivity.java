baa4d055b46ec5fd9c738c25c55303aade3e45e8
MujeebCool <mohdabdulmujeeb1999@gmail.com>
2020-05-28 12:36:09 +0530

    registration changes

--- app/src/main/java/com/alphabet/chitchat/RegistrationActivity.java
+++ app/src/main/java/com/alphabet/chitchat/RegistrationActivity.java
@@ -9,18 +9,13 @@ import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        tv=findViewById(R.id.tv);
-        tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(RegistrationActivity.this,SettingsActivity.class));
-            }
-        });
     }
 
+    public void user_login(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
+    }
 }
