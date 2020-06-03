7799e14a961da9efeff632179f57203fb02ecc64
MujeebCool <mohdabdulmujeeb1999@gmail.com>
2020-06-03 12:48:22 +0530

    firebase connected for authorised login

--- app/src/main/java/com/alphabet/chitchat/LoginActivity.java
+++ app/src/main/java/com/alphabet/chitchat/LoginActivity.java
@@ -1,21 +1,75 @@
 package com.alphabet.chitchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView tvregis;
+    private EditText email, password;
+    private Button login;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        tvregis = findViewById(R.id.tvregis);
+        email = findViewById(R.id.etemail_log);
+        password = findViewById(R.id.etpass_log);
+        login = findViewById(R.id.btnlogin);
+        firebaseAuth = FirebaseAuth.getInstance();
 
+        tvregis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+
+                if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+                    Toast.makeText(LoginActivity.this, "all fields are required", Toast.LENGTH_LONG).show();
+                } else {
+                    firebaseAuth.signInWithEmailAndPassword(txt_email, txt_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                                Toast.makeText(LoginActivity.this, "Login success...", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Login failed...", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 
-    public void new_user(View view) {
-        startActivity(new Intent(this, RegistrationActivity.class));
+    private void Login(String email, String password) {
+
     }
 }
--- app/src/main/java/com/alphabet/chitchat/RegistrationActivity.java
+++ app/src/main/java/com/alphabet/chitchat/RegistrationActivity.java
@@ -1,21 +1,97 @@
 package com.alphabet.chitchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    private EditText username, email, password;
+    private Button register;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+    private TextView login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        username = findViewById(R.id.etusername);
+        email = findViewById(R.id.etemail);
+        password = findViewById(R.id.etpass_reg);
+        firebaseAuth = FirebaseAuth.getInstance();
+        login = findViewById(R.id.tvlogin);
+        register = findViewById(R.id.btnregis);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_username = username.getText().toString();
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+                if (TextUtils.isEmpty(txt_username) || TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+                    Toast.makeText(RegistrationActivity.this, "All fields are required", Toast.LENGTH_LONG).show();
+                } else if (txt_password.length() < 8) {
+                    Toast.makeText(RegistrationActivity.this, "password must be atleast 8 characters", Toast.LENGTH_LONG).show();
+                } else
+                    Register(txt_username, txt_email, txt_password);
+            }
+        });
+
     }
 
-    public void user_login(View view) {
-        startActivity(new Intent(this, LoginActivity.class));
+    private void Register(final String username, String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                    String userid = firebaseUser.getUid();
+
+                    databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+                    HashMap<String, String> hashMap = new HashMap<>();
+                    hashMap.put("id", userid);
+                    hashMap.put("username", username);
+                    hashMap.put("imageURL", "default");
+
+                    databaseReference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                                Toast.makeText(RegistrationActivity.this, "Registration success", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(RegistrationActivity.this, "Registration failed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
